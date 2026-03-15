86da3349f3184f40344f7f8a0689468b39549ac4
kadfarias <kadson.farias@gmail.com>
2026-03-15 16:35:13 -0300

    Somando string de numeros

--- LogicTests/Lenght of string/Program.cs
+++ LogicTests/Lenght of string/Program.cs
@@ -1,6 +1,6 @@
 ﻿internal class Program
 {
-    private record TestInput(string imput);
+    private record TestInput(int n, int c, int k, string s);
 
     private static void Main(string[] args)
     {
@@ -24,14 +24,38 @@
     private static long Solution(TestInput input)
     {
 
+        //N  >= 1 && <= 100 000 -- String lenght
+        //C >= 1 && <= 10^9 -- Maximum number after split
+        //K >= 1 && <= 18 -- modulo
+        //S only digits 0-9 -- the string
 
+        int casaInicial = 1;
+
+
+        var valores = Enumerable.Range(0, input.s.Split(" ").Count()).
+            Select(i => new
+            {
+                valor = 1
+            });
+
+        return 1;
+    }
+
+    private static long Iterar(TestInput input, int casaInicial)
+    {
+        foreach (char c in input.s)
+        {
+            Console.WriteLine($"Character: {c}");
+        }
         return 1;
     }
 
     private static TestInput GetInput1()
     {
-        string imput = "0 ,1 ,2 ,3 ,4";
-
-        return new TestInput(imput);
+        int n = 5;
+        int c = 100;
+        int k = 10;
+        string s = "12321";
+        return new TestInput(n, c, k, s);
     }
 }
